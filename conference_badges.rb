3787f926636410b9a11ef5c68d97b17323bd5f62
Miriam Castillo <castillo.m.mc@gmail.com>
2018-04-05 22:15:37 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+def badge_maker(name)
+return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |attendees|
+    badges << badge_maker(attendees)
+  end
+ return badges
+end
+
+def assign_rooms(names)
+  room_assignments = []
+.each_with_index
+end
+return
+end
+
+def printer
+end
